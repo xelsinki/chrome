52c6dbf8570e653c84705f01fb87ab8fe8d4b769
Alex Pan <aleksandr.pantsesnyi@myy.haaga-helia.fi>
2016-12-08 09:52:38 +0000

    Copy of chrome module by James Netherton
    jamesnetherton/google_chrome

---
+++ config.pp
@@ -0,0 +1,50 @@
+class google_chrome::config() inherits google_chrome::params {
+
+  file { $google_chrome::params::defaults_file:
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "repo_add_once=\"false\"\nrepo_reenable_on_distupgrade=\"true\"\n",
+  }
+
+  case $::osfamily {
+    'RedHat': {
+      yumrepo { $google_chrome::params::repo_name:
+        name     => $google_chrome::params::repo_name,
+        enabled  => 1,
+        gpgcheck => 1,
+        baseurl  => $google_chrome::params::repo_base_url,
+        gpgkey   => $google_chrome::params::repo_gpg_key,
+      }
+    }
+    'Debian': {
+      Exec['apt_update'] -> Package["${google_chrome::params::package_name}-${google_chrome::version}"]
+
+      apt::source { $google_chrome::params::repo_name:
+        location => $google_chrome::params::repo_base_url,
+        release  => 'stable',
+        key      => {
+          id     => $google_chrome::params::repo_gpg_key_id,
+          source => $google_chrome::params::repo_gpg_key,
+        },
+        repos    => 'main',
+        include  => {
+          'src' => false
+        },
+      }
+    }
+    'Suse': {
+      zypprepo { $google_chrome::params::repo_name:
+        name     => $google_chrome::params::repo_name,
+        baseurl  => $google_chrome::params::repo_base_url,
+        enabled  => 1,
+        gpgcheck => 0,
+        type     => 'rpm-md',
+      }
+    }
+    default: {
+      fail("Unsupported operating system family ${::osfamily}")
+    }
+  }
+}
---
+++ params.pp
@@ -0,0 +1,27 @@
+class google_chrome::params() {
+  $ensure           = installed
+  $version          = 'stable'
+  $package_name     = 'google-chrome'
+  $repo_gpg_key     = 'https://dl.google.com/linux/linux_signing_key.pub'
+  $repo_gpg_key_id  = '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991'
+  $repo_name        = 'google-chrome'
+  $defaults_file    = '/etc/default/google-chrome'
+
+  case $::osfamily {
+    'RedHat' : {
+      if $::operatingsystemmajrelease == 6 {
+        fail('Operating system not supported by Google Chrome')
+      }
+      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
+    }
+    'Suse' : {
+      $repo_base_url = 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
+    }
+    'Debian': {
+      $repo_base_url = '[arch=amd64] http://dl.google.com/linux/chrome/deb/'
+    }
+    default: {
+      fail("Unsupported operating system family ${::osfamily}")
+    }
+  }
+}
